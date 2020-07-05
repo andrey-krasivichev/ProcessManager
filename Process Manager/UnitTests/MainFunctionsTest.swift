bed39932a0a61bf151ddb3c260aa1ec5e7ac6aad
Andrey Krasivichev <21ghost@mail.ru>
2020-07-05 13:02:53 +0300

    simple test is added

--- Process Manager/UnitTests/MainFunctionsTest.swift
+++ Process Manager/UnitTests/MainFunctionsTest.swift
@@ -7,27 +7,33 @@
 //
 
 import XCTest
+@testable import Process_Manager
 
 class MainFunctionsTest: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    var fetcher: ProcessItemsFetcher!
+    var itmesFetched: Bool = false
+    
+    override func setUp() {
+        self.fetcher = ProcessItemsFetcher()
+        fetcher.itemsFetchedBlock = { [weak self] (itemsById) in
+            self?.itmesFetched = itemsById.count > 0
         }
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        self.fetcher = nil
+        super.tearDown()
+    }
+    
+    func testExample() throws {
+        print(">> test")
+        
+        fetcher.startFetchEvents()
+        //self.wait(for: [], timeout: 3.0)
+        fetcher.finishFetchEvents()
+        XCTAssert(self.itmesFetched, "Processes must be fetched")
     }
 
 }
